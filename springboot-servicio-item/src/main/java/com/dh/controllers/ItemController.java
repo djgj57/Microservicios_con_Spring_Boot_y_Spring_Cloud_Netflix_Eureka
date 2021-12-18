b6be94b31dda18299bfdd9c3374229421086b078
davidGarcia <djgj57@hotmail.com>
2021-12-17 23:30:04 -0500

    sesion 6

--- springboot-servicio-item/src/main/java/com/dh/controllers/ItemController.java
+++ springboot-servicio-item/src/main/java/com/dh/controllers/ItemController.java
@@ -4,19 +4,44 @@ import com.dh.models.Item;
 import com.dh.models.Producto;
 import com.dh.models.service.IItemService;
 //import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
 
+@RefreshScope
 @RestController
 public class ItemController {
+
+    private final Logger log = org.slf4j.LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private Environment env;
+
+    @Autowired
+    private CircuitBreakerFactory cbFactory;
 
     @Autowired
     @Qualifier("serviceFeign")
 //    @Qualifier("serviceRestTemplate")
     private IItemService itemService;
+
+    @Value("${configuracion.texto}")
+    private String texto;
 
     @GetMapping("/listar")
     public List<Item> listar(@RequestParam(name="nombre", required = false) String nombre, @RequestHeader(name=
@@ -33,7 +58,30 @@ public class ItemController {
 //        return itemService.findById(id, cantidad);
 //    }
 
-    public Item metodoAlternativo(Long id, Integer cantidad){
+
+    @GetMapping("/ver/{id}/cantidad/{cantidad}")
+    public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad){
+        return cbFactory.create("items")
+                .run(() -> itemService.findById(id, cantidad), e -> metodoAlternativo(id,
+                        cantidad, e));
+    }
+
+    @CircuitBreaker(name = "items", fallbackMethod = "metodoAlternativo")
+    @GetMapping("/ver2/{id}/cantidad/{cantidad}")
+    public Item detalle2(@PathVariable Long id, @PathVariable Integer cantidad){
+        return itemService.findById(id, cantidad);
+    }
+
+    @CircuitBreaker(name = "items", fallbackMethod = "metodoAlternativo2")
+    @TimeLimiter(name = "items")
+    @GetMapping("/ver3/{id}/cantidad/{cantidad}")
+    public CompletableFuture<Item> detalle3(@PathVariable Long id,
+                                            @PathVariable Integer cantidad){
+        return CompletableFuture.supplyAsync(()-> itemService.findById(id, cantidad)) ;
+    }
+
+    public Item metodoAlternativo(Long id, Integer cantidad, Throwable e){
+        log.info("Error en el metodo alternativo: " + e.getMessage());
         Item item = new Item();
         Producto producto = new Producto();
         item.setCantidad(cantidad);
@@ -42,6 +90,32 @@ public class ItemController {
         producto.setPrecio(500.00);
         item.setProducto(producto);
         return item;
+    }
+
+    public CompletableFuture<Item> metodoAlternativo2(Long id, Integer cantidad, Throwable e){
+        log.info("Error en el metodo alternativo: " + e.getMessage());
+        Item item = new Item();
+        Producto producto = new Producto();
+        item.setCantidad(cantidad);
+        producto.setId(id);
+        producto.setNombre("Camara Sony");
+        producto.setPrecio(500.00);
+        item.setProducto(producto);
+        return CompletableFuture.supplyAsync(()->item);
+    }
+
+    @GetMapping("/obtener-config")
+    public ResponseEntity<?> obtenerConfig(@Value("${server.port}") String puerto){
+        log.info(texto);
+        Map<String, String> json = new HashMap<>();
+        json.put("texto", texto);
+        json.put("puerto", puerto);
+        if (env.getActiveProfiles().length > 0 && env.getActiveProfiles()[0].equals("dev")){
+            json.put("autor.nombre", env.getProperty("configuracion.autor.nombre"));
+            json.put("autor.email", env.getProperty("configuracion.autor.email"));
+        }
+
+        return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
     }
 
 }
